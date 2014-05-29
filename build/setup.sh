5e95f60e1cfa15b3392d0fdab2e7cbfaff7ecb42
nilp0inter <robertomartinezp@gmail.com>
2014-05-29 11:28:28 +0200

    Update setup.sh

--- build/setup.sh
+++ build/setup.sh
@@ -4,10 +4,10 @@
 /usr/bin/mysqld_safe > /dev/null 2>&1 &
 
 # Create Database
-mysql -uroot -proot -e "CREATE DATABASE a_database"
+mysql -uroot -proot -e "CREATE DATABASE develop"
 
 # Create (unsafe) HelpSpot user, who can connect remotely
-mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* to 'a_user'@'%' IDENTIFIED BY 'a_password';"
+mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* to 'develop'@'%' IDENTIFIED BY 'develop';"
 
 # Shutdown MySQL
 mysqladmin -uroot -proot shutdown
